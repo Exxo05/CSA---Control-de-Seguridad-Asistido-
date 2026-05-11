830378fa559da0f1d45c661d59698d9128009716
Exxo05 <mrosarioperezcejuela@gmail.com>
2026-05-11 11:18:50 +0200

    Delete CSA-V1.5/CSA-v1.5 directory

--- CSA-V1.5/CSA-v1.5/servicios/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import os
-
-def get_connection():
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.join(base_dir, "datos", "base.db")
-    return sqlite3.connect(db_path)
